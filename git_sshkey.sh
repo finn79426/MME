7bd5c80615553bce8dd04df3cbbd4373f045a94c
howpwn <finn79426@gmail.com>
2019-03-13 22:15:37 +0800

    Add ssh-key keygen script.

---
+++ git_sshkey.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -r -p "Do you want to generate a ssh-key (Y/n) ? : " confirm
+if [[ "$confirm" =~ ^([nN][oO]|[nN])+$ ]]
+then
+    exit
+else
+    ssh-keygen -t rsa -b 4096 -C "finn79426@gmail.com"
+    eval "$(ssh-agent -s)"
+    ssh-add -K ~/.ssh/id_rsa
+fi
+
+pbcopy < ~/.ssh/id_rsa.pub
+echo "id_rsa.pub => your clipboard"

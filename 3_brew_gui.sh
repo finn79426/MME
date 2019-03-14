92145e42798b15f56e17f50144d761dd4a4fb270
howpwn <finn79426@gmail.com>
2019-03-14 08:42:49 +0800

    Add setapp

--- 3_brew_gui.sh
+++ 3_brew_gui.sh
@@ -10,7 +10,7 @@ brew  cask  install  visual-studio-code
 brew  cask  install  burp-suite
 brew  cask  install  wireshark
 
-brew  cask  install  setapp
+brew  cask  install  setapp         # Better App Store
 brew  cask  install  clyang-welly   # BBS
 brew  cask  install  imageoptim     # 圖片壓縮神器
 brew  cask  install  keka           # 壓縮檔神器
